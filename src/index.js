564da30161fea42e944f251d482abda7bcb205be
Jinho Choi <dandyday31@gmail.com>
2021-09-04 11:28:51 +0900

    210903

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import { StrictMode } from "react";
+import react, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import "./styles.css";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+  const [item, setItem] = useState(10);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>+</button>
+      <button onClick={decrementItem}>-</button>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
